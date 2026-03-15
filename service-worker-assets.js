608aab19fedf44f7eba56c787a0882e9ee3bcec5
mattgene <mattgene@gmail.com>
2026-03-15 00:57:17 +0000

    Deploying to main from @ mattgene/BVRW2026@dd860e84e236ebe0db6a8d5a39af3196bb5b43b2 🚀

--- service-worker-assets.js
+++ service-worker-assets.js
@@ -1,5 +1,5 @@
 self.assetsManifest = {
-  "version": "bub5Kxo9",
+  "version": "B1GqMRFv",
   "assets": [
     {
       "hash": "sha256-Lx7YCf7dQwllUsj6ayM9EaqmpdGyh2Z6jXMKurRwKO8=",
@@ -42,11 +42,11 @@ self.assetsManifest = {
       "url": "_framework/BCrypt.Net-Next.wasm"
     },
     {
-      "hash": "sha256-S8P8R88hvnTwbEWcQmNmWFbPvBMD83XVdy9llYGLQdM=",
+      "hash": "sha256-KTRT0NxgDWDEY8Cr9k2D0rPP1O7TKk7VKyYUY7Ti9ug=",
       "url": "_framework/BVRWeb.DataAccess.wasm"
     },
     {
-      "hash": "sha256-zHUe296bIJxqRKDn0pP5sesFa84S60+AgMg/AvBCvW0=",
+      "hash": "sha256-zgpdFvgs3cdLuSA2MwkNcMefVAoQQMILlBmmCxqKsoM=",
       "url": "_framework/BVRWeb.SiteWork.wasm"
     },
     {
@@ -1070,7 +1070,7 @@ self.assetsManifest = {
       "url": "_framework/blazor.webassembly.js"
     },
     {
-      "hash": "sha256-9humt0qgr34kZl8IpALeB8JcGeiPFZgWOFD5zmvJ1WU=",
+      "hash": "sha256-pVAC03qRzLwGy/pGeElDbFd2D5dVglvuxy4wh4gH22k=",
       "url": "_framework/dotnet.js"
     },
     {
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: bub5Kxo9 */
+/* Manifest version: B1GqMRFv */
 // 監聽安裝事件
 self.addEventListener('install', event => {
     console.log('Service Worker: Installing...');
